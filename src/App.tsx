6928d43a70f352902622b981279aa3780bbdf31b
HamedSadim1 <100290950+HamedSadim1@users.noreply.github.com>
2023-09-21 18:48:56 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+const slotNumbers = [0, 1, 2, 3, 4];
+const getImgTekst = (slotNumber: number) => {
+  switch (slotNumber) {
+    case 0:
+      return <img src={require("./img/slot-cherry.png")} width="50"></img>;
+      break;
+    case 1:
+      return (
+        <img src={require("./img/slot-lemon.png")} width="50" alt=""></img>
+      );
+      break;
+    case 2:
+      return <img src={require("./img/slot-melon.png")} width="50"></img>;
+      break;
+    case 3:
+      return <img src={require("./img/slot-prune.png")} width="50"></img>;
+      break;
+    case 4:
+      return <img src={require("./img/slot-seven.png")} width="50"></img>;
+      break;
+  }
+};
+let slotWin =
+  slotNumbers[0] === slotNumbers[1] &&
+  slotNumbers[1] === slotNumbers[2] &&
+  slotNumbers[2] === slotNumbers[3] &&
+  slotNumbers[3] === slotNumbers[4];
+let randomSlotNumbers = slotNumbers.map((slotNumber) =>
+  Math.floor(Math.random() * 5)
+);
+console.log(randomSlotNumbers);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div>
+        <h1>Slotmachine</h1>
+        {slotNumbers.map((slotNumber: number, index: number) => (
+          <span key={index}>{getImgTekst(slotNumber)}</span>
+        ))}
+        <br />
+
+        {randomSlotNumbers.map((slotNumber: number, index: number) => (
+          <span key={index}>{getImgTekst(slotNumber)}</span>
+        ))}
+        {slotWin ? <h2>You win</h2> : <h2>You lose</h2>}
+        <button type="button" className="btn btn-secondary btn-sm">
+          <a className="link-dark" href="/">
+            Refresh
+          </a>
+        </button>
+      </div>
+    </React.Fragment>
   );
 }
-
 export default App;
